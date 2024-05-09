b9bf3a742d8c68dfbb6b2a5413eaa1e0d0f9c1a1
JAY\Laptop <jaykshirsagar6@gmail.com>
2024-05-09 13:18:31 +0300

    Adding uploud file interface

--- licenta/src/app/component/lab1/lab1.component.ts
+++ licenta/src/app/component/lab1/lab1.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-lab1',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './lab1.component.css'
 })
 export class Lab1Component {
-
+  pdfUrl: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer){
+    let url = 'assets/lab1.pdf';
+    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+  
 }
